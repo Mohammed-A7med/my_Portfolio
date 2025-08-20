8f4eaddfc75595999da477c6513c198f245752d8
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-08-20 23:32:10 +0300

    feat(ProjectCard): add tech badges section and links for Live Demo & GitHub

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -13,16 +13,19 @@ export default function Projects() {
           link="https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4"
           title="Project Management System"
           description="Project Management System with role-based auth, CRUD, drag-and-drop tasks, themes, and real-time tracking. Built with React, TypeScript, Context API, Bootstrap, and modern UI libraries for a smooth, responsive experience."
+          listOfTech={["React", "TypeScript", "HTML", "Bootstrap"]}
         />
         <ProjectCard
           link="https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4"
           title="Project Management System"
           description="Project Management System with role-based auth, CRUD, drag-and-drop tasks, themes, and real-time tracking. Built with React, TypeScript, Context API, Bootstrap, and modern UI libraries for a smooth, responsive experience."
+          listOfTech={["React", "TypeScript", "HTML", "Bootstrap"]}
         />
         <ProjectCard
           link="https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4"
           title="Project Management System"
           description="Project Management System with role-based auth, CRUD, drag-and-drop tasks, themes, and real-time tracking. Built with React, TypeScript, Context API, Bootstrap, and modern UI libraries for a smooth, responsive experience."
+          listOfTech={["React", "TypeScript", "HTML", "Bootstrap"]}
         />
       </div>
     </section>
---
+++ src/components/Projects/TechBadge.jsx
@@ -0,0 +1,7 @@
+export default function TechBadge({ label }) {
+  return (
+    <span className="bg-[#0582ca] rounded-full py-0.5 px-4 text-sm text-white">
+      {label}
+    </span>
+  );
+}
