40ea991e5d79ff12bbd8f6da5a465eda02222588
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-08-28 21:10:38 +0300

    feat(ui): update contact icons and add Download CV button

--- src/components/Ui-icons/LocationIcon.jsx
+++ src/components/Ui-icons/LocationIcon.jsx
@@ -2,13 +2,18 @@ export default function LocationIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 384 512"
       width="24"
       height="24"
-      fill="currentColor"
-      aria-hidden="true"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-map-pin h-5 w-5 text-primary"
     >
-      <path d="M168 0C75.1 0 0 75.1 0 168c0 87.3 144 311.1 168 344 2.4 3.2 7.6 3.2 10 0 24-32.9 168-256.7 168-344C346 75.1 270.9 0 178 0H168zM192 240c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72-32.2 72-72 72z" />
+      <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"></path>
+      <circle cx="12" cy="10" r="3"></circle>
     </svg>
   );
 }
