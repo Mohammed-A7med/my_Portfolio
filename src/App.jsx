f906068fbb236897ff36d0eade1c166077efb129
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-09-09 16:19:47 +0300

    feat(contact): integrate EmailJS for sending contact form messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useEffect } from "react";
+
 import "./App.css";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
+import emailjs from "@emailjs/browser";
 
 function App() {
+  useEffect(() => {
+    emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+  }, []);
+
   return (
     <>
       <Navbar />
       <Home />
-      <Projects/>
-      <Contact/>
+      <Projects />
+      <Contact />
     </>
   );
 }
